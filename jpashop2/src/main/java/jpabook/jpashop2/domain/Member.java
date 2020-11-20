53684cbe1cd7252fb9d25e0fd08095b6a74e88ce
MiraeChoi <pnenv@naver.com>
2020-11-21 00:54:48 +0900

    add member repository

--- jpashop2/src/main/java/jpabook/jpashop2/domain/Member.java
+++ jpashop2/src/main/java/jpabook/jpashop2/domain/Member.java
@@ -10,12 +10,17 @@ import java.util.List;
 @Entity
 @Getter @Setter
 public class Member {
+
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
     private String name;
+
     @Embedded
     private Address address;
+
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
+
 }
