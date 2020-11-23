1a77497d0e2a71dbbbd798f0fa8de83edb217cf2
MiraeChoi <pnenv@naver.com>
2020-11-24 04:05:47 +0900

    update member service test

--- jpashop2/src/test/java/jpabook/jpashop2/service/MemberServiceTest.java
+++ jpashop2/src/test/java/jpabook/jpashop2/service/MemberServiceTest.java
@@ -3,39 +3,54 @@ package jpabook.jpashop2.service;
 import jpabook.jpashop2.domain.Member;
 import jpabook.jpashop2.repository.MemberRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 public class MemberServiceTest {
-    @Autowired
-    MemberService memberService;
 
-    @Autowired
-    MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
 
     @Test
+    @Rollback(value = false)
     public void 회원가입() throws Exception {
-        //Given
+        //given
         Member member = new Member();
-        member.setName("kim");
-        //When
-        Long saveId = memberService.join(member);
-        //Then
-        assertEquals(member, memberRepository.findOne(saveId));
+        member.setName("damgom1");
+
+        //when
+        Long savedId = memberService.join(member);
+        
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
     }
 
     @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
-        //Given
+        //given
         Member member1 = new Member();
-        member1.setName("kim");
+        member1.setName("damgom1");
+
         Member member2 = new Member();
-        member2.setName("kim");
-        //When
+        member2.setName("damgom1");
+
+        //when
         memberService.join(member1);
-        memberService.join(member2); //예외가 발생해야 한다.
-        //Then
+        memberService.join(member2); //예외가 발생해야 함~!
+
+        //then
         fail("예외가 발생해야 한다.");
     }
 }
