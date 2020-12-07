616b9a9a5e74e14e6c4b04123124c77e3744a7b2
MiraeChoi <pnenv@naver.com>
2020-12-07 23:56:21 +0900

    add member join api

--- jpashop2/src/main/java/jpabook/jpashop2/repository/OrderRepository.java
+++ jpashop2/src/main/java/jpabook/jpashop2/repository/OrderRepository.java
@@ -25,7 +25,7 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
-        String jpql = "select o from Order o join o.member";
+        String jpql = "select o from Order o join o.member m";
         boolean isFirstCondition = true;
 
         //주문 상태 검색
