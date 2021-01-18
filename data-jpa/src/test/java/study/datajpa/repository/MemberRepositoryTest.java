78a1eb30a042bea6127bc06d8fa83f3bc15c3775
MiraeChoi <pnenv@naver.com>
2021-01-18 22:07:37 +0900

    add projections

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -326,7 +326,34 @@ class MemberRepositoryTest {
 
         assertThat(result.get(0).getUsername()).isEqualTo("gom1");
     }
+
+    @Test
+    public void projections() {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("gom1", 0, teamA);
+        Member m2 = new Member("gom2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        List<NestedClosedProjections> result = memberRepository.findProjectionsByUsername("gom1", NestedClosedProjections.class);
+
+        for (NestedClosedProjections nestedClosedProjections : result) {
+            String username = nestedClosedProjections.getUsername();
+            System.out.println("username = " + username);
+            String teamName = nestedClosedProjections.getTeam().getName();
+            System.out.println("teamName = " + teamName);
+        }
+    }
 }
+
+
 
 
 
